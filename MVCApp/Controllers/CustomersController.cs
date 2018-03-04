9c69f0f82dae23dc76833cedf07159767ec2fce6
AnChp17 <chanirudh@outlook.com>
2018-03-03 19:20:02 -0500

    Intial Commit

---
+++ MVCApp/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            List<Customer> customersList = new List<Customer>()
+            {
+                new Customer(){ID = 1, Name = "Andy"},
+                new Customer(){ID = 2, Name = "Randy"}
+            };
+
+            return customersList;
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.ID == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+    }
+}
---
+++ MVCApp/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Models;
+using MVCApp.ViewModel;
+
+namespace MVCApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Die Hard" };
+            var customers = new List<Customer>()
+            {
+                new Customer(){ID = 1, Name = "A"},
+                new Customer(){ID = 2, Name = "B"}
+            };
+
+            var viewmodal = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewmodal);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            List<Movie> moviesList = new List<Movie>()
+            {
+                new Movie(){ID = 1, Name = "Die Hard1"},
+                new Movie(){ID = 2, Name = "Die Hard2"}
+            };
+
+            return moviesList;
+        }
+    }
+}
